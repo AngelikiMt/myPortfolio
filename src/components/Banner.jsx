79076d420c42feb196f4c91d84e47c3ab0c077fa
AngelikiMt <matta.d.angeliki@gmail.com>
2025-08-19 11:32:23 +0300

    Adding contact section and editing download my CV button

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,8 @@
 import { Container, Row, Col } from "react-bootstrap";
-import { BsArrowRightCircle } from "react-icons/bs";
 import myPhoto from '../assets/myPhoto.png';
 import '../components/Banner.css';
 import myCV from '/src/assets/CV-Angeliki_Matta.pdf';
+import { FiDownload } from "react-icons/fi";
 
 const Banner = () => {
 
@@ -15,7 +15,7 @@ const Banner = () => {
                     <h1>Hi, I am Angeliki <br/> Software Developer</h1>
                     <p>I am currently pursuing an MSc in Enterprice Software Systems Development. My professional journey has taken me to a Web Development internship, freelance Web Development work and a wide range of personal projects and online courses that supported built my strong programming fundamentals.<br/><br/>I specialize in both frontend and backend development transforming ideas into functional responsive digital experiences using clean code and modern technologies.</p>
                     <a href={myCV} download target="_blank" rel="noopener noreferrer" className="cv-button">
-                        <button>Get my CV <BsArrowRightCircle size={30}></BsArrowRightCircle></button>
+                        <button>Download my CV <FiDownload size={30} /></button>
                     </a>
                     </Col>
                     <Col xs={12} md={6} xl={5}> 
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,69 @@
 import { Container, Row, Col } from "react-bootstrap";
 import '../components/Contact.css';
+import { useState } from "react";
+import TrackVisibility from 'react-on-screen';
 
 const Contact = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        subject: '',
+        message: ''
+    });
+
+    const [status, setStatus] = useState('');
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setStatus('Sending...');
+
+        try {
+            const response = await fetch("http://localhost:5173/contact", {
+                method: 'POST',
+                headers: { ' Content-Type': 'application/json' },
+                body: JSON.stringify(formData)
+            });
+
+            const result = await respomse.json();
+            if (result.code === 200) {
+                setStatus('Message sent successfully!');
+                setFormData({ name: '', email: '', subject: '', message: '' });
+            } else {
+                setStatus('Something went wrong. Try again later.');
+            }
+        } catch (error) {
+            setStatus('Error sending message.');
+        }
+    };
+
     return (
-        <section className="contact" id="contact">
+       <section className="contact" id="contact">
             <Container>
-                <Row>
-                    <Col>
-                        <h2>Contact</h2>
-                    </Col>
+                <Row className="justify-content-center">
+                    <TrackVisibility>
+                        {({ isVisible }) => (
+                            <div className={`contact-box ${isVisible ? "show" : ""}`}>
+                                <h2 className="contact-title">CONTACT ME</h2>
+                                <form onSubmit={handleSubmit}>
+                                    <input type="text" class="form-group" name="name" placeholder="Name" value={formData.name} onChange={handleChange} required />
+                                    <input type="email" class="form-group" name="email" placeholder="Email" value={formData.email} onChange={handleChange} required />
+                                    <input type="text" class="form-group" name="subject" placeholder="Subject" value={formData.subject} onChange={handleChange} required />
+                                    <textarea class="form-group" name="message" placeholder="Message" rows="6" value={formData.message} onChange={handleChange} required ></textarea>
+                                    <button class="contact-btn" type="submit" className="send-btn">Send Message</button>
+                                    {status && <p className="status">{status}</p>}
+                                </form>
+                            </div>
+                        )}
+                    </TrackVisibility>
                 </Row>
             </Container>
-        </section>
-    );
+         </section>
+
+        );
 }
 
 export default Contact;

ab1adae753e6e893ced45a08c1b5140e54ed9413
AngelikiMt <matta.d.angeliki@gmail.com>
2025-11-19 10:13:10 +0200

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -14,7 +14,10 @@ const Banner = () => {
                     <Col xs={12} md={6} xl={5}>
                     <span className="tagline">Welcome to my portfolio</span>
                     <h1>Hi, I am Angeliki <br/> Software Developer</h1>
-                    <p>I am currently pursuing an MSc in Enterprice Software Systems Development. My professional journey has taken me to a Web Development internship, freelance Web Development work and a wide range of personal projects and online courses that supported built my strong programming fundamentals.<br/><br/>I specialize in both frontend and backend development transforming ideas into functional responsive digital experiences using clean code and modern technologies.</p>
+                    <p>I am currently pursuing an MSc in Enterprise Software Systems Development. My professional journey has taken me to a Web Development internship, 
+                        freelance Web Development work, and a wide range of personal projects and online courses that helped build my strong programming fundamentals.<br/><br/>
+                        Alongside my experience in frontend and backend development, I have developed strong skills in SQL, data modeling, and data visualization tools such 
+                        as Tableau, allowing me to transform raw datasets into clear, meaningful insights. I specialize in creating functional, responsive digital experiences using clean code, modern technologies, and data-driven decision-making where needed.</p>
                     <a href="#contact" rel="noopener noreferrer" className="cv-button">
                         <button>Drop your message <FiDownload size={30} /></button>
                     </a>

8020bb2d3c5fe5b219e3d4ded12a1defbf09ed46
AngelikiMt <matta.d.angeliki@gmail.com>
2026-01-13 14:26:47 +0200

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -39,7 +39,7 @@ const Projects = () => {
             title: 'Quality Remodeling Website',
             description: "A full-stack website, designed for informing users about company's projects and other information. Built with Node.js, Express.js, React and bootstrap",
             imgUrl: qm,
-            gitHubUrl: 'https://newqm.onrender.com/'
+            gitHubUrl: 'https://www.qualityremodeling.gr'
         },
         {
             title: 'RetailHub Capstone Project',
